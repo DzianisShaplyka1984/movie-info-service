a07a70008aae4c07f753427d854e4d94a61dd796
Dzianis Shaplyka <dzianis_shaplyka@epam.com>
2020-05-21 10:46:04 +0300

    add api-key properties, add RestTemplate bean

--- src/main/java/my/movie/movieinfoservice/controllers/MovieController.java
+++ src/main/java/my/movie/movieinfoservice/controllers/MovieController.java
@@ -1,14 +1,26 @@
 package my.movie.movieinfoservice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import my.movie.movieinfoservice.model.entity.Movie;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
+    @Value("${api.key}")
+    private String apiKey;
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public MovieController(final RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @RequestMapping("/{movieId}")
     public Movie getMovie(@PathVariable("movieId") final String movieId) {
